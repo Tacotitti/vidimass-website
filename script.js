e2cc5f68e093b808b5934da60204a61cb55ebba2
Tacotitti <nunesloeweiuri@gmail.com>
2026-04-04 22:59:16 +0200

    Remove Packages section from homepage and navigation - pricing only on preisliste.html

--- script.js
+++ script.js
@@ -15,7 +15,6 @@ document.addEventListener('DOMContentLoaded', function() {
         mobileMenu.innerHTML = `
             <a href="#features" data-i18n="nav_features">Features</a>
             <a href="tiktok-charting.html">TikTok Charting</a>
-            <a href="#packages" data-i18n="nav_packages">Packages</a>
             <a href="preisliste.html">Preisliste</a>
             <a href="contact.html" data-i18n="nav_contact">Kontakt</a>
             
